0fe4f43744e62aa9435fba1f35979a8f2c008ac2
spartaksun <2697024@gmail.com>
2016-07-02 00:01:40 +0300

    Added bundle class

---
+++ ContextLoggerBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by A.Belyakovskiy.
+ * Date: 7/1/16
+ * Time: 11:57 PM
+ */
+
+namespace Spartaksun\ContextLoggerBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContextLoggerBundle extends Bundle
+{
+
+}
